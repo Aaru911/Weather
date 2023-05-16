9b511a8146d6a156b2ea232cfe29804649c99a8f
Aaru <workingaaru@gmail.com>
2023-05-16 12:34:54 +0530

    Weather App 1.0

---
+++ index.js
@@ -0,0 +1,33 @@
+var button = document.getElementById("get-location");
+
+button.addEventListener('click', async () => {
+    navigator.geolocation.getCurrentPosition(success, fail);
+})
+
+async function success(position) {
+    var text=document.getElementById("show-text");
+    var img = document.getElementById("img");
+    var type= document.getElementById("type");
+
+    const result= await get_data(position.coords.latitude,position.coords.longitude);
+    console.log(result)
+    let str=result.location.name
+    str2=str.concat(" <br>Current Temprature is-",result.current.temp_c);
+
+    text.innerHTML = str2;
+    img.src=result.current.condition.icon;
+    img.style.visibility = "visible";
+    type.innerHTML=result.current.condition.text;
+
+}
+
+function fail(position) {
+    console.log("Error:",position);
+}
+
+async function get_data(lat,long){
+    const promis=await fetch(
+        `http://api.weatherapi.com/v1/forecast.json?key=5aace33d334f4d609e650639231605&q=${lat},${long}&aqi=yes`
+        );
+        return await promis.json();
+} 
